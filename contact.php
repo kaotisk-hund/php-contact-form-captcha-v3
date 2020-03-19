70b7f69269dd521ff5e67d9f1e5c6d429a13e32b
kaotisk <kaotisk@arching-kaos.tk>
2020-03-19 03:55:58 +0200

    Cleanup a bit, also made recaptcha optional.

--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
  * Title: PHP Contact form with Captcha v3
@@ -14,101 +13,85 @@
  * License: GNU GPL v3
  */
 
-
-$sitename = 'YOUR SITE NAME HERE';
-$sent_to = 'YOUR EMAIL HERE';
+include 'config.php';
 
 // Check if form was submitted:
-if($_POST != NULL && $_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['recaptcha_response']) && $_POST['email']!= NULL){
-    // Assign
-    $user = $_POST['user'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-	// Build POST request:
-    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-    $recaptcha_secret = 'YOUR SECRET KEY HERE';
-    $recaptcha_response = $_POST['recaptcha_response'];
-	
-    
-    // Format subject
-    $subject = '['.$sitename.'] '.$fullname . ' sent you an email using your web form at your site.';
-    // Format email
-    $full_message = 'Name : '. $user. "\r\n" . 'Email : ' .$email. "\r\n".'Message : ' .$message;
-	
-    // Make and decode POST request:
-    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
-    $recaptcha = json_decode($recaptcha);
+if($_POST != NULL && $_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['email']!= NULL){
+	// Assign
+	$user = $_POST['user'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	if (recaptcha_enabled){
+		// Build POST request:
+		$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+		$recaptcha_response = $_POST['recaptcha_response'];
+	}
 
-    // Take action based on the score returned:
-    if ($recaptcha->score >= 0.5) {
-        // Verified - send email
-	echo '<div class="small-12 column label success">Mail sent</div>';
-	mail($sent_to, $subject, $full_message);
-    } else {
-        // Not verified - show form error
-	echo '<div class="small-12 column label alert">Mail failed, please try again or use another form of contact.</div>';
-    }    
+	// Format subject
+	$subject = '['.$sitename.'] '.$user . ' sent you an email using your web form at your site.';
+	// Format email
+	$full_message = 'Name : '. $user. "\r\n" . 'Email : ' .$email. "\r\n".'Message : ' .$message;
+	if (recaptcha_enabled){
+		// Make and decode POST request:
+		$recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+		$recaptcha = json_decode($recaptcha);
+
+		// Take action based on the score returned:
+		if ($recaptcha->score >= 0.5) {
+			// Verified - send email
+			$status = '<div class="small-12 column label success">Mail sent</div>';
+			mail($sent_to, $subject, $full_message);
+		} else {
+			//  Not verified - show form error
+		   echo '<div class="small-12 column label alert">Mail failed, please try again or use another form of contact.</div>';
+		}
+	} else {
+		$status = '<div class="small-12 column label success">Mail sent</div>';
+		mail($sent_to, $subject, $full_message);
+	}
 }
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title><?php echo $sitename; ?> | Contact form</title>
-</head>
-<style type="text/css">
-	.row .large-12 .small-12 {
-		max-width: 100%;
-		width: 100%;
-	}
-	.large-6 .small-6 {
-		width: 50%;
-	}
-	.success {
-		background-color: green;
-		color: white;
-	}
-	.alert {
-		background-color: red;
-		color: black;
-	}
-</style>
-<body>
-<div id="contact">
-    <!-- Form -->
-    <div class="row">
-        <div class="large-12 small-12">
-            <form method="post" action="">
-                <div class="row">
-                    <div class="large-6 small-6">
-                        <label>Your name
-                            <input name="user" type="text" placeholder="e.g. John Smith"/>
-                        </label>
-                    </div>
-                    <div class="large-6 small-6">
-                        <label>Email address
-                                <input name="email" type="text" placeholder="e.g. mail@example.com"/>
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="large-12 small-12">
-                        <label>Message
-                            <textarea name="message" placeholder="Type your message/questions here"></textarea>
-                        </label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="large-6 small-6">
-                        <input type="submit" class="button success" value="Send"/>
-                    </div>
-                    <div class="large-6 small-6 columns">
-                        <input type="reset" class="button alert" value="Clear form"/>
-                    </div>
-                </div>
-		<input type="hidden" name="recaptcha_response" id="recaptchaResponse">
-            </form>
-        </div>
-    </div>
-</div>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $sitename; ?> | Contact form</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+	<div id="contact">
+		<div class="status"><?php if ($status != NULL){ echo $status; } ?>
+		<!-- Form -->
+		<h1>Contact form</h1>
+		<p><?php echo $description; ?></p>
+		<form method="post" action="">
+			<label>Your name
+				<input name="user" type="text" placeholder="e.g. John Smith"/>
+			</label>
+			<br>
+			<label>Email address
+					<input name="email" type="text" placeholder="e.g. mail@example.com"/>
+			</label>
+			<br>
+			<label>Message<br>
+				<textarea name="message" placeholder="Type your message/questions here"></textarea>
+			</label>
+			<br>
+			<input type="submit" class="button success" value="Send"/>
+			<input type="reset" class="button alert" value="Clear form"/>
+			<?php
+				if (recaptcha_enabled) {
+					echo '<input type="hidden" name="recaptcha_response" id="recaptchaResponse">';
+				}
+			?>
+		</form>
+		<!-- End of Form -->
+		</div>
+	</div>
+	<footer>
+		<p>
+			Source code: <a href="https://github.com/kaotisk-hund/php-contact-form-captcha-v3">@github</a>
+		</p>
+	</footer>
+	</body>
 </html>
